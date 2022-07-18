0d735292ba09023b6203ad983a7db6129567c93f
Tmeng <tonymengt@gmail.com>
2022-07-17 21:37:37 -0400

    changed shop input from strings to integer for ease of access

--- assets/js/game.js
+++ assets/js/game.js
@@ -141,20 +141,17 @@ var fightOrSkip = function() {
     var shop= function () {
         // ask player what they'd like to do
         var shopOptionPrompt = window.prompt (
-            "Would you like to REFIL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+            "Would you like to REFIL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for 'REFILL', 2 for 'UPGRADE', or 3 for 'LEAVE' to make a choice."
         )
-
+        shopOptionPrompt = parseInt(shopOptionPrompt);
         switch (shopOptionPrompt){
-            case "refill":
-            case "REFILL":
+            case 1:
                 playerInfo.refillHealth()
                 break;
-            case "upgrade":
-            case "UPGRADE":
+            case 2:
                 playerInfo.upgradeAttack()
                 break;
-            case "leave":
-            case "LEAVE":
+            case 3:
                 window.alert("Leaving the store.");
                 // do nothing, so function will end
                 break;
